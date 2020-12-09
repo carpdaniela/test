0f4097939acadc10b78a9b1b976bdb9419c6423b
carpdaniela <iulianadana97@gmail.com>
2020-12-09 17:10:37 +0200

    update controller part.18

--- controller.js
+++ controller.js
@@ -77,17 +77,9 @@ app.controller("addController",function ($scope,$http) {
 		$http(req_del).then(function(response) {
 			$scope.products = response.data;
 			
-			var req = {
-				 method: 'GET',
-				 url: 'http://localhost:1323/todos',
-				 headers: {
-				   'Origin': "localhost:1323"
-				 }
-			}
-
-			$http(req).then(function(response) {
-				$scope.products = response.data;
-			}, onError);
+			
+			var index = $scope.products.indexOf(msg);
+			$scope.products.splice(index, 1);  
 			
 		}, onError);
 		
